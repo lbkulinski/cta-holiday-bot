0c654d61f44274bf907c1b4f06f21d6545c4c303
Logan <rashes_lineage02@icloud.com>
2022-11-19 17:28:39 -0600

    Added JavaDoc comments to Train and TrainDeserializer

--- src/main/java/com/cta4j/Train.java
+++ src/main/java/com/cta4j/Train.java
@@ -4,6 +4,18 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
 import java.time.LocalDateTime;
 
+/**
+ * A train of the Chicago Transit Authority.
+ *
+ * @author Logan Kulinski, rashes_lineage02@icloud.com
+ * @version November 19, 2022
+ * @param route the route of this {@link Train}
+ * @param run the run of this {@link Train}
+ * @param station the station of this {@link Train}
+ * @param destination the destination of this {@link Train}
+ * @param predictionTime the prediction time of this {@link Train}
+ * @param arrivalTime the arrival time of this {@link Train}
+ */
 @JsonDeserialize(using = TrainDeserializer.class)
 public record Train(String route, int run, String station, String destination, LocalDateTime predictionTime,
     LocalDateTime arrivalTime) {
